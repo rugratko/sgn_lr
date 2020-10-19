1c794185faabf6ca5ef74d03eee75b37d506ed78
Ostap Borshchov <31808441+rugratko@users.noreply.github.com>
2020-10-19 15:53:04 +0300

    Update task_1.py

--- lr_3/task_1.py
+++ lr_3/task_1.py
@@ -14,10 +14,6 @@ def counter(start, end, step):
     for i in range(end, start - 1, step): #опять компенсируем, только уже для первого числа
         print(i, end = ' ')
 
-# start = int(input('Введите число начала отсчета: '))
-# end = int(input('Введите число конца отсчета: '))
-# step = int(input('Введите шаг отсчета: '))
-
 start = -209
 end = 194
 step = 13
