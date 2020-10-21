48bf03ebcd1c4625e1f99e4ed00732146916646f
Ostap Borshchov <31808441+rugratko@users.noreply.github.com>
2020-10-21 16:21:14 +0300

    Create exp2.py

---
+++ exp/exp2.py
@@ -0,0 +1,9 @@
+print('Task 7')
+x=int(input('Введите первое число: '))
+y=int(input('Введите второе число: '))
+if x>y:
+    print(x-y)
+elif x<y:
+    print(y-x)
+elif x==y:
+    print("Числа одинаковые")
