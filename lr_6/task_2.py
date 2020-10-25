c2560206263ff646b2cd0243ca5b1f9869b72774
Ostap Borshchov <31808441+rugratko@users.noreply.github.com>
2020-10-25 19:36:54 +0300

    132

--- lr_6/task_2.py
+++ lr_6/task_2.py
@@ -1,17 +1,19 @@
 '''
 Вариант 11:
-Вам дана последовательность строк. Выведите строки, содержащие текст в круглых скобках.
+Вам дана последовательность строк. 
+Выведите строки, содержащие текст в круглых скобках.
 '''
 
 contents = []
 while True:
-    text = input('Введите строку или ОТМЕНА для отмены')
+    text = input('Введите строку или ОТМЕНА для отмены: ')
     if text == 'ОТМЕНА':
         break
     else:
         contents.append(text)
 
-for line in l:
+for line in contents:
     for i in "()":
         if i in line:
-        	print(line)
+            print(line)
+        break
