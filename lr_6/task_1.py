db7a02969af711e4c3758344e7452caa18559195
Ostap Borshchov <31808441+rugratko@users.noreply.github.com>
2020-10-25 19:25:56 +0300

    Create task_1.py

---
+++ lr_6/task_1.py
@@ -0,0 +1,18 @@
+'''
+Вариант 11:
+Вам дана последовательность строк. Выведите строки, содержащие текст в круглых скобках.
+'''
+
+contents = []
+while True:
+    text = input('Введите строку или ОТМЕНА для отмены: ')
+    if text == 'ОТМЕНА':
+        break
+    else:
+        contents.append(text) 
+        
+for line in contents:
+    if 'Рисунок' in line:
+        new_line = line.replace('Рисунок', 'Фото')ы
+        print(new_line)
+    
