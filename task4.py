88b42862acd1f64f550d81a131de58a1c7103c42
Ostap Borshchov <31808441+rugratko@users.noreply.github.com>
2020-11-20 17:47:53 +0300

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,10 @@
+import pickle
+
+with open('File130', 'rb') as f:
+    data_new = pickle.load(f)
+    
+key = 70
+answer = data_new.get(key)
+print('Значение с ключом 70: ', answer)
+
+print('Содержание словаря целиком', data_new)
